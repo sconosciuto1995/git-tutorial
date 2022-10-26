6c8c58de041315f92bfa4b89cc07d05dd92dbe6d
Rami Mougaida <rami.mougaida@gmail.com>
2022-10-26 16:51:50 +0200

    coding challenge 2

--- src/code.c
+++ src/code.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <espl_lib.h>
 int main() {
+   int num ;
+   char ch;
    // printf() displays the string inside quotation
-   printf("Hello ESPL");
+ do{
+   printf("give a number\n");
+    scanf("%d",&num);
+    printf ("%s\n",num_to_words( num));
+    printf ("Do you want to repeat the operation Y/N: ");
+    scanf (" %c", &ch);
+       }
+        while (ch == 'y' || ch == 'Y');
    return 0;
 }
 
